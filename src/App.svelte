e495237f3eebfdd08c0b5339196f1f4cd3269dfb
Anthony Destenay <anthony.destenay@gmail.com>
2020-04-17 13:27:01 +0200

    UI prototype

--- src/App.svelte
+++ src/App.svelte
@@ -1,106 +1,160 @@
 <svelte:options tag="lbm-toolbar" accessors={false} />
 
 <script>
-	import { onMount } from 'svelte';
+	let show = false;
 
-	let time = new Date();
-
-	// these automatically update when `time`
-	// changes, because of the `$:` prefix
-	$: hours = time.getHours();
-	$: minutes = time.getMinutes();
-	$: seconds = time.getSeconds();
-
-	onMount(() => {
-		const interval = setInterval(() => {
-			time = new Date();
-		}, 1000);
-
-		return () => {
-			clearInterval(interval);
-		};
-	});
+	function toggleToolbar() {
+		show = !show;
+	}
 </script>
 
 <style>
-	svg {
+	a#button {
+		position: fixed;
+		top: 10%;
+		left: 0;
+		z-index: 10001;
+		width: 3em;
+		height: 3em;
+		display: flex;
+		border-radius: 0 4px 4px 0;
+		border: 0;
+		background: #fff;
+		box-shadow: 0 0 10px rgba(0, 0, 0, .5);
+		padding: 0;
+		cursor: pointer;
+		align-items: center;
+		justify-content: center;
+	}
+
+	#overlay {
+		background: rgba(0, 0, 0, .75);
+		position: fixed;
 		width: 100%;
+		height: 100%;
+		top: 0;
+		bottom: 0;
+		left: 0;
+		right: 0;
+		z-index: 10000;
+	}
+
+	#wrap {
+		display: flex;
+		align-items: center;
+		justify-content: center;
 		height: 100%;
 	}
 
-	.clock-face {
-		stroke: #333;
-		fill: white;
+	#tools {
+		display: grid;
+	  height: 80%;
+	  grid-template-rows: repeat(7, 1fr);
+	  grid-template-columns: repeat(2, 1fr);
+	  grid-gap: 1em;
+		max-width: 60em;
+		width: 100%;
+		padding: 0;
+		margin: 0 4em;
 	}
 
-	.minor {
-		stroke: #999;
-		stroke-width: 0.5;
+	#tools li {
+		display: flex;
+	  justify-content: center;
+	  align-items: center;
+		grid-row: span 1;
+    grid-column: span 1;
 	}
 
-	.major {
-		stroke: #333;
-		stroke-width: 1;
+	#tools li.high {
+		grid-row: span 2;
+    grid-column: span 1;
 	}
 
-	.hour {
-		stroke: #333;
+	#tools li.large {
+		grid-row: span 1;
+    grid-column: span 2;
 	}
 
-	.minute {
-		stroke: #666;
+	#tools li a {
+		text-decoration: none;
+		color: #fff;
+		font-weight: bold;
+		display: flex;
+		width: 100%;
+		height: 100%;
+		align-content: center;
+		align-items: center;
+		justify-content: center;
+		flex-wrap: wrap;
+		background: red;
+		border-radius: 4px;
 	}
 
-	.second, .second-counterweight {
-		stroke: rgb(180,0,0);
+	#tools li a span {
+		display: block;
+		width: 100%;
+		text-align: center;
 	}
 
-	.second-counterweight {
-		stroke-width: 3;
+	#tools li a .subtitle {
+		font-weight: normal;
+		font-size: .8em;
 	}
 </style>
 
-<svg viewBox='-50 -50 100 100'>
-	<circle class='clock-face' r='48'/>
+<a on:click="{toggleToolbar}" id="button">
+	{#if show}
+		&times;
+	{:else}
+		LBM
+	{/if}
+</a>
 
-	<!-- markers -->
-	{#each [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55] as minute}
-		<line
-			class='major'
-			y1='35'
-			y2='45'
-			transform='rotate({30 * minute})'
-		/>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit est fugiat eaque, deleniti ducimus ut autem quibusdam ab voluptatibus optio nemo nulla? Repellat iste ab rem, quia doloremque reiciendis saepe!</p>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit est fugiat eaque, deleniti ducimus ut autem quibusdam ab voluptatibus optio nemo nulla? Repellat iste ab rem, quia doloremque reiciendis saepe!</p>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit est fugiat eaque, deleniti ducimus ut autem quibusdam ab voluptatibus optio nemo nulla? Repellat iste ab rem, quia doloremque reiciendis saepe!</p>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit est fugiat eaque, deleniti ducimus ut autem quibusdam ab voluptatibus optio nemo nulla? Repellat iste ab rem, quia doloremque reiciendis saepe!</p>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit est fugiat eaque, deleniti ducimus ut autem quibusdam ab voluptatibus optio nemo nulla? Repellat iste ab rem, quia doloremque reiciendis saepe!</p>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit est fugiat eaque, deleniti ducimus ut autem quibusdam ab voluptatibus optio nemo nulla? Repellat iste ab rem, quia doloremque reiciendis saepe!</p>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit est fugiat eaque, deleniti ducimus ut autem quibusdam ab voluptatibus optio nemo nulla? Repellat iste ab rem, quia doloremque reiciendis saepe!</p>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit est fugiat eaque, deleniti ducimus ut autem quibusdam ab voluptatibus optio nemo nulla? Repellat iste ab rem, quia doloremque reiciendis saepe!</p>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit est fugiat eaque, deleniti ducimus ut autem quibusdam ab voluptatibus optio nemo nulla? Repellat iste ab rem, quia doloremque reiciendis saepe!</p>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit est fugiat eaque, deleniti ducimus ut autem quibusdam ab voluptatibus optio nemo nulla? Repellat iste ab rem, quia doloremque reiciendis saepe!</p>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit est fugiat eaque, deleniti ducimus ut autem quibusdam ab voluptatibus optio nemo nulla? Repellat iste ab rem, quia doloremque reiciendis saepe!</p>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit est fugiat eaque, deleniti ducimus ut autem quibusdam ab voluptatibus optio nemo nulla? Repellat iste ab rem, quia doloremque reiciendis saepe!</p>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit est fugiat eaque, deleniti ducimus ut autem quibusdam ab voluptatibus optio nemo nulla? Repellat iste ab rem, quia doloremque reiciendis saepe!</p>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit est fugiat eaque, deleniti ducimus ut autem quibusdam ab voluptatibus optio nemo nulla? Repellat iste ab rem, quia doloremque reiciendis saepe!</p>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit est fugiat eaque, deleniti ducimus ut autem quibusdam ab voluptatibus optio nemo nulla? Repellat iste ab rem, quia doloremque reiciendis saepe!</p>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit est fugiat eaque, deleniti ducimus ut autem quibusdam ab voluptatibus optio nemo nulla? Repellat iste ab rem, quia doloremque reiciendis saepe!</p>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit est fugiat eaque, deleniti ducimus ut autem quibusdam ab voluptatibus optio nemo nulla? Repellat iste ab rem, quia doloremque reiciendis saepe!</p>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit est fugiat eaque, deleniti ducimus ut autem quibusdam ab voluptatibus optio nemo nulla? Repellat iste ab rem, quia doloremque reiciendis saepe!</p>
 
-		{#each [1, 2, 3, 4] as offset}
-			<line
-				class='minor'
-				y1='42'
-				y2='45'
-				transform='rotate({6 * (minute + offset)})'
-			/>
-		{/each}
-	{/each}
-
-	<!-- hour hand -->
-	<line
-		class='hour'
-		y1='2'
-		y2='-20'
-		transform='rotate({30 * hours + minutes / 2})'
-	/>
-
-	<!-- minute hand -->
-	<line
-		class='minute'
-		y1='4'
-		y2='-30'
-		transform='rotate({6 * minutes + seconds / 10})'
-	/>
-
-	<!-- second hand -->
-	<g transform='rotate({6 * seconds})'>
-		<line class='second' y1='10' y2='-38'/>
-		<line class='second-counterweight' y1='10' y2='2'/>
-	</g>
-</svg>
+{#if show}
+	<div id="overlay">
+		<div id="wrap">
+			<ul id="tools">
+				<li class="large"><a href="#!" target="_blank"><span class="title">Site principal</span></a></li>
+				<li><a href="#!" target="_blank"><span class="title">Outil 1</span></a></li>
+				<li class="high">
+					<a href="#!" target="_blank">
+						<span class="title">Outil 3</span>
+						<span class="subtitle">Subtitle</span>
+					</a>
+				</li>
+				<li><a href="#!" target="_blank"><span class="title">Outil 2</span></a></li>
+				<li class="high">
+					<a href="#!" target="_blank">
+						<span class="title">Outil 4</span>
+						<span class="subtitle">Subtitle</span>
+					</a>
+				</li>
+				<li><a href="#!" target="_blank"><span class="title">Outil 5</span></a></li>
+				<li><a href="#!" target="_blank"><span class="title">Outil 6</span></a></li>
+				<li><a href="#!" target="_blank"><span class="title">Outil 7</span></a></li>
+				<li><a href="#!" target="_blank"><span class="title">Outil 8</span></a></li>
+				<li class="large"><a href="#!" target="_blank"><span class="title">RSS</span></a></li>
+			</ul>
+		</div>
+	</div>
+{/if}
